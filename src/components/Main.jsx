84a4cc2553dd52ad79c50a2bccb1f84b9f3a9e6e
Ismael Loko <lokoismael30@gmail.com>
2025-05-30 13:00:40 -0400

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main className="flex flex-col gap-y-3 mx-auto size-[350px] rounded-[10px] bg-[#F5F5F5] text-[#2B283A] font-Inter">
+      <h1>Tenzies</h1>
+    </main>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import Main from "./components/Main";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <Main />
+  </>
+);
