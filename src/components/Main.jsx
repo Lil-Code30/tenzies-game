7cae97fcbad3242ad6ee2cafe2f09f963bd36a82
Ismael Loko <lokoismael30@gmail.com>
2025-05-30 13:33:42 -0400

    Die componenet part 1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
 export default function Main() {
   return (
-    <main className="flex flex-col gap-y-3 mx-auto size-[350px] rounded-[10px] bg-[#F5F5F5] text-[#2B283A] font-Inter">
-      <h1>Tenzies</h1>
+    <main className="flex flex-col items-center py-10 gap-y-3 mx-3 sm:mx-auto mt-20 size-full md:mt-50 md:size-[600px] rounded-[10px] bg-[#F5F5F5]  font-Inter">
+      <h1 className="text-5xl font-bold text-[#2B283A]">Tenzies</h1>
+      <p className="text-center text-2xl text-[#4A4E74] font-normal w-[70%] line-clamp-3">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="grid grid-cols-5 gap-"></div>
     </main>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import Main from "./components/Main";
+import Main from "./components/Main.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
